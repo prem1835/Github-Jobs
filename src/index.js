e1c815a29808d8e85647712876e5ff2817a2ebe1
Prem Mali <premmali1835@gmail.com>
2020-07-04 15:43:09 +0530

    added support for scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+import App from "./component/App";
 
 ReactDOM.render(
   <React.StrictMode>
